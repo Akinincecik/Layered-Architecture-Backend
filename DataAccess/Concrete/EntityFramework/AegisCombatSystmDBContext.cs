d7247c088677eb092ce7e56e768d74efbb8bb750
akindev <akindev@AKINDEV>
2024-12-19 22:48:03 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/AegisCombatSystmDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class AegisCombatSystmDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=AKINDEV;Database=Aegis_Combat_System_Ex;Integrated Security=True;TrustServerCertificate=True");
+            //optionsBuilder.UseSqlServer(@"Data Source=127.0.0.1;Initial Catalog=Aegis_Combat_System_Ex;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
